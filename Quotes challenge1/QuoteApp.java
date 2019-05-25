200af4adb8d5a8bde6ed09d7ef64d688b00885c7
Sujal31 <sujal.patel007@gmail.com>
2019-05-24 22:25:45 -0700

    First Commit

---
+++ Quotes challenge1/QuoteApp.java
@@ -0,0 +1,12 @@
+
+public class QuoteApp
+{
+    public static void main(String[] args) 
+    {
+        // Create all objects and call all methods that you
+        // need here. You have freedom as to how you want to
+        // interact with the user. 
+    }
+
+    // Write your aux methods here:
+}
---
+++ Quotes challenge1/QuoteFile.java
@@ -0,0 +1,12 @@
+import java.io.*;
+// load the classes that you need. 
+
+public class QuoteFile
+{
+    
+    QuoteFile(String file_name)
+    {
+        File file = new File(file_name);
+        ...
+    }
+}
